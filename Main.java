d07ce885b6a4e8bcf0cc9bfe4667c105c6bf6793
dlyons5 <dominic.lyons@patelhs.org>
2020-10-30 13:43:40 +0000

    Made an for loop and added more numbers

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 class Main {
   public static void main(String[] args) {
     
-  int[] scores = { 0, 1, 2, 3};
+  int[] scores = { 0, 1, 2, 3, 4, 5, 6};
        
       for (int i = 0; i < scores.length; i++)
       {
