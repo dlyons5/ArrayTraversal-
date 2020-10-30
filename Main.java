5b68d61d7e582d32ca39a6ee88a2d72b202dfaa4
dlyons5 <dominic.lyons@patelhs.org>
2020-10-30 13:41:30 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    
+  int[] scores = { 0, 1, 2, 3};
+       
+      for (int i = 0; i < scores.length; i++)
+      {
+          System.out.println(  scores[i] );
+      }
+   }
+ }
+    
+    
+    
+    
